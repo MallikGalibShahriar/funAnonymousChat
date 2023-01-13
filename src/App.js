a1a6dacb6a7cf7202408f927cf265654962c0556
Mallik Galib Shahriar <mgalibshahriar@gmail.com>
2023-01-13 12:42:33 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import './App.css'
+
+
+function ChatApp() {
+  const [user, setUser] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    setMessages([...messages, { user, message }]);
+    setMessage("");
+  };
+
+  return (
+    <div>
+      <h1>Chat App</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          User:
+          <input
+            type="text"
+            value={user}
+            onChange={event => setUser(event.target.value)}
+          />
+        </label>
+        <label>
+          Message:
+          <input
+            type="text"
+            value={message}
+            onChange={event => setMessage(event.target.value)}
+          />
+        </label>
+        <button type="submit">Send</button>
+      </form>
+      <div>
+        {messages.map((m, index) => (
+          <div key={index}>
+            <b>{m.user}:</b> {m.message}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default ChatApp;
